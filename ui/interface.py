6d3b3748062d216b38a8bd9b42ad73a21fa8a17f
AndrewDya <andrewdya@gmail.com>
2023-08-04 20:33:29 +0600

    ui changes

--- ui/interface.py
+++ ui/interface.py
@@ -12,22 +12,30 @@ class UI:
         self.root = tk.Tk()
         self.root.title("Приложение для учета расходов")
 
+        self.root.geometry("400x300")
+
         self.main_frame = tk.Frame(self.root)
         self.main_frame.pack()
 
         self.show_menu()
 
     def show_menu(self):
+        for widget in self.main_frame.winfo_children():
+            widget.destroy()
+
         menu_label = tk.Label(self.main_frame, text="Выберите действие:")
         menu_label.pack()
 
-        add_button = tk.Button(self.main_frame, text="Добавить расход", command=self.add_expense)
+        add_button = tk.Button(self.main_frame, text="Добавить расход",
+                               command=self.add_expense)
         add_button.pack()
 
-        view_button = tk.Button(self.main_frame, text="Просмотреть расходы", command=self.view_expenses)
+        view_button = tk.Button(self.main_frame, text="Просмотреть расходы",
+                                command=self.view_expenses)
         view_button.pack()
 
-        budget_button = tk.Button(self.main_frame, text="Отслеживание бюджета", command=self.budget_tracking)
+        budget_button = tk.Button(self.main_frame, text="Отслеживание бюджета",
+                                  command=self.budget_tracking)
         budget_button.pack()
 
     def add_expense(self):
@@ -57,7 +65,19 @@ class UI:
     def budget_tracking(self):
         total_budget = self.budget.get_total_budget()
         remaining_budget = self.budget.get_remaining_budget()
-        BudgetTrackingView.show_budget_info(total_budget, remaining_budget)
+
+        for widget in self.main_frame.winfo_children():
+            widget.destroy()
+
+        total_budget_label = tk.Label(self.main_frame, text=f"Общий бюджет: {total_budget}")
+        total_budget_label.pack()
+
+        remaining_budget_label = tk.Label(self.main_frame, text=f"Остаток бюджета: {remaining_budget}")
+        remaining_budget_label.pack()
+
+        back_button = tk.Button(self.main_frame, text="Назад", command=self.show_menu)
+        if back_button:
+            back_button.pack()
 
     def run(self):
         self.root.mainloop()
