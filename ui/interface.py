3b2551a56b4791dd0910ee707c5bc745aba82821
AndrewDya <andrewdya@gmail.com>
2023-08-04 19:37:53 +0600

    UI class edit

--- ui/interface.py
+++ ui/interface.py
@@ -1,63 +1,63 @@
-
-from expenses.category import Category
+import tkinter as tk
+from tkinter import messagebox
 from expenses.budget import Budget
 from expenses.espense import Expense
+from ui.views import ExpenseInputView, ExpenseListView, BudgetTrackingView
 
 
 class UI:
     def __init__(self):
-        self.budget = Budget(total_budget=1000)  # Задайте начальный бюджет
+        self.budget = Budget(total_budget=5000)
+
+        self.root = tk.Tk()
+        self.root.title("Приложение для учета расходов")
+
+        self.main_frame = tk.Frame(self.root)
+        self.main_frame.pack()
+
+        self.show_menu()
 
     def show_menu(self):
-        print("1. Добавить расход")
-        print("2. Просмотреть расходы")
-        print("3. Отслеживание бюджета")
-        print("0. Выход")
+        menu_label = tk.Label(self.main_frame, text="Выберите действие:")
+        menu_label.pack()
+
+        add_button = tk.Button(self.main_frame, text="Добавить расход", command=self.add_expense)
+        add_button.pack()
+
+        view_button = tk.Button(self.main_frame, text="Просмотреть расходы", command=self.view_expenses)
+        view_button.pack()
+
+        budget_button = tk.Button(self.main_frame, text="Отслеживание бюджета", command=self.budget_tracking)
+        budget_button.pack()
 
     def add_expense(self):
-        amount = float(input("Введите сумму расхода: "))
-        date = input("Введите дату расхода (ГГГГ-ММ-ДД): ")
-        category_name = input("Введите категорию расхода: ")
-        description = input("Введите описание расхода: ")
+        expense_details = ExpenseInputView.get_expense_details()
 
-        category = self.budget.get_category(category_name)
+        category = self.budget.get_category(expense_details[2])
         if category is None:
-            print("Категория не найдена. Создайте категорию.")
+            BudgetTrackingView.category_not_found()
             return
 
-        expense = Expense(amount, date, category, description)
+        expense = Expense(*expense_details, category)
         category.add_expense(expense)
-        print("Расход успешно добавлен.")
+        messagebox.showinfo("Добавление расхода", "Расход успешно добавлен.")
 
     def view_expenses(self):
-        category_name = input("Введите название категории (или оставьте пустым для просмотра всех расходов): ")
-        category = self.budget.get_category(category_name)
-
-        if category:
-            expenses = category.get_expenses()
+        import tkinter.simpledialog
+        category_name = tkinter.simpledialog.askstring("Просмотр расходов",
+                                                       "Введите название категории (или оставьте пустым для просмотра всех расходов):")
+        if category_name:
+            category = self.budget.get_category(category_name)
+            expenses = category.get_expenses() if category else []
         else:
-            expenses = [expense for cat in self.budget.categories.values() for expense in cat.get_expenses()]
-
-        for expense in expenses:
-            print(expense)
+            expenses = [expense for cat in self.budget.categories.values() for
+                        expense in cat.get_expenses()]
+        ExpenseListView.show_expenses(expenses)
 
     def budget_tracking(self):
-        print(f"Общий бюджет: {self.budget.get_total_budget()}")
-        print(f"Остаток бюджета: {self.budget.get_remaining_budget()}")
+        total_budget = self.budget.get_total_budget()
+        remaining_budget = self.budget.get_remaining_budget()
+        BudgetTrackingView.show_budget_info(total_budget, remaining_budget)
 
     def run(self):
-        while True:
-            self.show_menu()
-            choice = input("Выберите действие: ")
-
-            if choice == "1":
-                self.add_expense()
-            elif choice == "2":
-                self.view_expenses()
-            elif choice == "3":
-                self.budget_tracking()
-            elif choice == "0":
-                print("Выход из приложения.")
-                break
-            else:
-                print("Неверный выбор. Попробуйте снова.")
+        self.root.mainloop()
