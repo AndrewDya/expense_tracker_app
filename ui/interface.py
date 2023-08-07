87e189ecfd78a92ab3206a5e51a39163ddfff398
AndrewDya <andrewdya@gmail.com>
2023-08-07 19:01:54 +0600

    choose category iu added

--- ui/interface.py
+++ ui/interface.py
@@ -24,7 +24,7 @@ class UI:
             widget.destroy()
 
         balance_label = tk.Label(self.main_frame, text="Баланс: $5000",
-                                 font=("Helvetica", 10), anchor="w")
+                                 font=("Helvetica", 10, "bold"), anchor="w")
         balance_label.pack()
 
         account_category_label = tk.Label(self.main_frame, text="Счета:",
@@ -44,7 +44,42 @@ class UI:
             button.pack(side="left", padx=6)
 
     def choose_category(self):
-        pass
+        for widget in self.main_frame.winfo_children():
+            widget.destroy()
+
+        title_label = tk.Label(self.main_frame, text="Выберите категорию:",
+                               font=("Helvetica", 10, "bold"))
+        title_label.pack(padx=10, pady=10)
+
+        categories = ["Еда", "Транспорт", "Развлечения", "Здоровье", "Одежда",
+                      "Путешествия", "Образование", "Дом", "Техника", "Прочее"]
+
+        for i in range(0, len(categories), 2):
+            row_frame = tk.Frame(self.main_frame)
+            row_frame.pack(fill="x", padx=10, pady=5)
+
+            for j in range(2):
+                index = i + j
+                if index < len(categories):
+                    category = categories[index]
+
+                    button = tk.Button(
+                        row_frame, text=category, anchor="center",
+                        width=15,
+                        command=lambda cat=category: self.category_selected(
+                            cat),
+                        relief=tk.RAISED, pady=8, bg="lightgreen", fg="black",
+                        font=("Helvetica", 10, "bold")
+                    )
+                    button.pack(side="left", padx=8)
+
+        for text, command in self.button_data:
+            button = tk.Button(
+                self.main_frame, text=text, anchor="w", command=command,
+                relief=tk.RAISED, pady=7, bg="lightblue", fg="black",
+                font=("Helvetica", 10)
+            )
+            button.pack(side="left", padx=7)
 
     def choose_operation(self):
         pass
